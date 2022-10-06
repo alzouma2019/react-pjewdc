56705b942dc1ab801d3b7acefe24dc72b4908f39
alzouma2019 <57457748+alzouma2019@users.noreply.github.com>
2022-10-06 11:52:34 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import axios from 'axios';
+import './style.css';
+
+const baseURL = 'https://jsonplaceholder.typicode.com/posts';
 
 export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(`${baseURL}/1`).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function updatePost() {
+    axios
+      .put(`${baseURL}/1`, {
+        title: 'Hello World',
+        body: 'This is a new post.',
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  if (!post) return 'No post!';
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={updatePost}>Update Post </button>
     </div>
   );
 }
